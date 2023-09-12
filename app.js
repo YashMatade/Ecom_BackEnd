e54bff5e39155391f0ae214a06edd81d94e0d236
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2023-09-12 06:38:50 -0700

    Other changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { connectionDB } = require("./db/connection")
 const indexRoutes = require("./routes/index");
-
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json())
@@ -12,7 +12,8 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use("/", indexRoutes)
 
-connectionDB(); const port = 5000
-app.listen(port, () => {
+connectionDB();
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log("server is running on 5000");
 });
