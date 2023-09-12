7776777b829dc762bb5fb0647c3ae15c8a1560f0
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2023-09-12 06:02:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use("/", indexRoutes)
+app.use("/", indexRoutes);
+app.get("/", (req, res)=>{
+    res.send("Server is running");
+})
 
 connectionDB(); const port = 5000
 app.listen(port, () => {
